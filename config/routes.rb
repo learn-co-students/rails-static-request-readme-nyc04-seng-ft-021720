cd38da4beda40bb80c1fa9b2fe29d8f74f5e5964
Ryan Seit <ryan@ryanseit.com>
2020-03-13 10:54:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 'about' is the HTTP verb
+  ## controller action 'static#about' tells Rails to route through the static controllers 'about' action
+  ### 'action' is the Rails term for 'method' in Ruby
+  get 'about', to: 'static#about'
+
 end
